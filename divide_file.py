537479471b3426e1936d8b808da1bf60efafdec1
Francisco Dominguez <fdominguezg@Franciscos-MBP.attlocal.net>
2023-08-22 22:22:20 -0400

    compare two files modified

--- divide_file.py
+++ divide_file.py
@@ -1,31 +1,36 @@
 import boto3
 import math
 import pandas as pd
+import io
 
 s3 = boto3.resource('s3')
 bucket_name = 'your-bucket-name'
 source_key = 'source_directory/large_file.csv'  # Replace with the actual source path
 target_directory = 'target_directory/'  # Replace with the actual target directory
-chunk_size = 100 * 1024 * 1024  # 100MB in bytes
+target_chunk_size = 100 * 1024 * 1024  # 100MB in bytes (target chunk size)
+
+def split_csv_into_chunks(file_content, chunk_size):
+    chunks = []
+    remaining = file_content
+    while len(remaining) > chunk_size:
+        chunk, remaining = remaining[:chunk_size], remaining[chunk_size:]
+        chunks.append(chunk)
+    chunks.append(remaining)
+    return chunks
 
 def lambda_handler(event, context):
     bucket = s3.Bucket(bucket_name)
     obj = bucket.Object(source_key)
     total_bytes = obj.content_length
-    total_chunks = int(math.ceil(total_bytes / chunk_size))
+    total_chunks = int(math.ceil(total_bytes / target_chunk_size))
     
-    header_line = None
+    file_content = obj.get()['Body'].read()
+    chunks = split_csv_into_chunks(file_content, target_chunk_size)
     
-    for chunk_number in range(1, total_chunks + 1):
-        start_byte = (chunk_number - 1) * chunk_size
-        end_byte = min(chunk_number * chunk_size - 1, total_bytes - 1)
-        byte_range = f"bytes={start_byte}-{end_byte}"
-        
-        chunk_data = obj.get(Range=byte_range)['Body'].read()
-        chunk_df = pd.read_csv(pd.compat.StringIO(chunk_data.decode('utf-8')))
-        
-        if header_line is None:
-            header_line = chunk_data[:chunk_data.find(b'\n') + 1]
+    header_line = chunks[0][:chunks[0].find(b'\n') + 1]
+    
+    for chunk_number, chunk_data in enumerate(chunks, start=1):
+        chunk_df = pd.read_csv(io.StringIO(chunk_data.decode('utf-8')))
         
         target_key = f'{target_directory}small_chunk_{chunk_number}.csv'
         target_data = header_line + chunk_df.to_csv(index=False).encode('utf-8')
