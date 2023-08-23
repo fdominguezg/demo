ffe68b39949bebde51590fbe708dfbc5aeb87e14
Francisco Dominguez <fdominguezg@Franciscos-MBP.attlocal.net>
2023-08-22 21:21:06 -0400

    compare two files modified

--- divide_file.py
+++ divide_file.py
@@ -2,15 +2,16 @@ import boto3
 import math
 import pandas as pd
 
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
 bucket_name = 'your-bucket-name'
 source_key = 'source_directory/large_file.csv'  # Replace with the actual source path
 target_directory = 'target_directory/'  # Replace with the actual target directory
 chunk_size = 100 * 1024 * 1024  # 100MB in bytes
 
 def lambda_handler(event, context):
-    response = s3.get_object(Bucket=bucket_name, Key=source_key)
-    total_bytes = response['ContentLength']
+    bucket = s3.Bucket(bucket_name)
+    obj = bucket.Object(source_key)
+    total_bytes = obj.content_length
     total_chunks = int(math.ceil(total_bytes / chunk_size))
     
     header_line = None
@@ -20,7 +21,7 @@ def lambda_handler(event, context):
         end_byte = min(chunk_number * chunk_size - 1, total_bytes - 1)
         byte_range = f"bytes={start_byte}-{end_byte}"
         
-        chunk_data = response['Body'].read(Range=byte_range)
+        chunk_data = obj.get(Range=byte_range)['Body'].read()
         chunk_df = pd.read_csv(pd.compat.StringIO(chunk_data.decode('utf-8')))
         
         if header_line is None:
@@ -29,4 +30,4 @@ def lambda_handler(event, context):
         target_key = f'{target_directory}small_chunk_{chunk_number}.csv'
         target_data = header_line + chunk_df.to_csv(index=False).encode('utf-8')
         
-        s3.put_object(Bucket=bucket_name, Key=target_key, Body=target_data)
+        s3.Object(bucket_name, target_key).put(Body=target_data)
